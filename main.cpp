88bcf8169c524bdcc760f2645903dc8e0b4d26d6
jungkbugk <jsstory97@naver.com>
2019-07-05 21:20:34 +0900

    Add Sub Function

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include "sub.h"
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	printf("%d", sub(n));
+}
---
+++ sub.cpp
@@ -0,0 +1,5 @@
+#include "sub.h"
+
+int sub(n){
+	return n*(n+1)/2*(-1);
+}
